1d14cbc27d2c5f8c24a19e9504af8ffa350b3d3c
Danny Wong <wong.danny29@gmail.com>
2019-01-18 13:16:46 -0800

    added new stylesheet

--- controllers/vglcontroller.js
+++ controllers/vglcontroller.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const router = express.Router(); 
+
+const Events = require('../models/events');
+const Teams = require('../models/teams');
+
+router.get('/', (req, res) => {
+	res.render('home.ejs');
+});
