e9ecbbd8ad6bf042c2acae6b837298239cc31207
Danny Wong <wong.danny29@gmail.com>
2019-02-19 13:29:22 -0800

    setting up user accounts

--- server.js
+++ server.js
@@ -5,10 +5,22 @@ const app = express();
 
 const bodyParser = require('body-parser'); 
 const methodOverride = require('method-override');
+const cors           = require('cors');
+const session        = require('express-session')
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
 
-const vglController = require('./controllers/vglcontroller'); 
-const adminController = require('./controllers/admincontroller');
+const corsOptions = {
+  origin: 'https://localhost:3000', 
+
+  credentials: true, // This allows the session cookie to be sent back and forth
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
 
 
 app.use(express.static('public'));
@@ -17,10 +29,16 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 
 
-app.use('/', vglController);
-app.use('/admin', adminController);
+
+const vglController = require('./controllers/vglcontroller'); 
+const adminController = require('./controllers/admincontroller');
+const authController = require('./controllers/authcontroller');
+
+app.use('/api/v1/', vglController);
+app.use('/api/v1//admin', adminController);
+app.use('/api/v1/auth', authController);
 
 
-app.listen(process.env.PORT || 3000, () => {
-	console.log("Server is listening in port 3000");
+app.listen(process.env.PORT || 9000, () => {
+	console.log("Server is listening in port 9000");
 });
